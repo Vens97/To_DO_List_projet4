5e143786cb1fc4ca01a6bfbbae1728a8b55cc726
Altieri <steven_altieri@yahoo.com>
2024-07-12 13:20:56 +0200

    implémentez la logique de filtrage avec les commentaire

--- ToDoList/Sources/ToDoListView.swift
+++ ToDoList/Sources/ToDoListView.swift
@@ -1,22 +1,29 @@
 import SwiftUI
 
+// Déclaration de la structure ToDoListView conforme au protocole View
 struct ToDoListView: View {
+    // Propriété observée pour le ViewModel
     @ObservedObject var viewModel: ToDoListViewModel
-    @State private var newTodoTitle = ""
-    @State private var isShowingAlert = false
-    @State private var isAddingTodo = false
-    
-    // New state for filter index
+    // États pour la gestion des entrées utilisateur et de l'interface
+    @State private var newTodoTitle = "" // Stocke le titre du nouveau todo
+    @State private var isShowingAlert = false // Contrôle l'affichage de l'alerte
+    @State private var isAddingTodo = false // Contrôle l'affichage de la vue d'ajout de todo
+
+    // Nouvel état pour l'index du filtre
     @State private var filterIndex = 0 {
+        // Applique le filtre lorsque l'index change
         didSet {
             viewModel.applyFilter(at: filterIndex)
         }
     }
 
+    // Corps de la vue
     var body: some View {
+        // Vue de navigation pour fournir une barre de navigation
         NavigationView {
+            // Disposition verticale
             VStack {
-                // Filter selector
+                // Picker pour sélectionner le filtre de tâches
                 Picker("Filter", selection: $filterIndex) {
                     Text("All").tag(0)
                     Text("Done").tag(1)
@@ -25,24 +32,29 @@ struct ToDoListView: View {
                 .pickerStyle(SegmentedPickerStyle())
                 .padding()
 
-                // List of tasks
+                // Liste des tâches
                 List {
+                    // Itération sur les éléments de la liste de tâches
                     ForEach(viewModel.toDoItems) { item in
                         HStack {
+                            // Bouton pour marquer la tâche comme terminée ou non
                             Button(action: {
                                 viewModel.toggleTodoItemCompletion(item)
                             }) {
+                                // Icône pour indiquer l'état de la tâche
                                 Image(systemName: item.isDone ? "checkmark.circle.fill" : "circle")
                                     .resizable()
                                     .frame(width: 25, height: 25)
                                     .foregroundColor(item.isDone ? .green : .primary)
                             }
+                            // Texte de la tâche avec style conditionnel
                             Text(item.title)
                                 .font(item.isDone ? .subheadline : .body)
                                 .strikethrough(item.isDone)
                                 .foregroundColor(item.isDone ? .gray : .primary)
                         }
                     }
+                    // Gestion de la suppression des tâches
                     .onDelete { indices in
                         indices.forEach { index in
                             let item = viewModel.toDoItems[index]
@@ -51,27 +63,30 @@ struct ToDoListView: View {
                     }
                 }
 
-                // Sticky bottom view for adding todos
+                // Vue fixe en bas pour ajouter de nouvelles tâches
                 if isAddingTodo {
                     HStack {
+                        // Champ de texte pour entrer le titre de la nouvelle tâche
                         TextField("Enter Task Title", text: $newTodoTitle)
                             .padding(.leading)
 
                         Spacer()
-
+  
+                        // Bouton pour ajouter la nouvelle tâche
                         Button(action: {
                             if newTodoTitle.isEmpty {
-                                isShowingAlert = true
+                                isShowingAlert = true // Affiche l'alerte si le titre est vide
                             } else {
                                 viewModel.add(
                                     item: .init(
                                         title: newTodoTitle
                                     )
                                 )
-                                newTodoTitle = "" // Reset newTodoTitle to empty.
-                                isAddingTodo = false // Close the bottom view after adding
+                                newTodoTitle = "" // Réinitialise le titre du nouveau todo
+                                isAddingTodo = false // Ferme la vue d'ajout après ajout
                             }
                         }) {
+                            // Icône pour le bouton d'ajout
                             Image(systemName: "plus.circle.fill")
                                 .resizable()
                                 .frame(width: 30, height: 30)
@@ -84,7 +99,7 @@ struct ToDoListView: View {
                     .padding(.horizontal)
                 }
 
-                // Button to toggle the bottom add view
+                // Bouton pour afficher ou masquer la vue d'ajout
                 Button(action: {
                     isAddingTodo.toggle()
                 }) {
@@ -101,11 +116,12 @@ struct ToDoListView: View {
 
             }
             .navigationBarTitle("To-Do List")
-            .navigationBarItems(trailing: EditButton())
+            .navigationBarItems(trailing: EditButton()) // Bouton pour passer en mode édition
         }
     }
 }
 
+// Prévisualisation de la vue avec des données simulées
 struct ToDoListView_Previews: PreviewProvider {
     static var previews: some View {
         ToDoListView(
--- ToDoList/Sources/ToDoListViewModel.swift
+++ ToDoList/Sources/ToDoListViewModel.swift
@@ -2,36 +2,36 @@ import SwiftUI
 
 final class ToDoListViewModel: ObservableObject {
     // MARK: - Private properties
-
+    
     private let repository: ToDoListRepositoryType
     private var allToDoItems: [ToDoItem] = []
-
+    
     // MARK: - Init
-
+    
     init(repository: ToDoListRepositoryType) {
         self.repository = repository
         let items = repository.loadToDoItems()
         self.allToDoItems = items
         self.toDoItems = items
     }
-
+    
     // MARK: - Outputs
-
+    
     /// Publisher for the list of to-do items.
     @Published var toDoItems: [ToDoItem] = [] {
         didSet {
             repository.saveToDoItems(toDoItems)
         }
     }
-
+    
     // MARK: - Inputs
-
+    
     // Add a new to-do item with priority and category
     func add(item: ToDoItem) {
         allToDoItems.append(item)
         applyFilter(at: currentFilterIndex)
     }
-
+    
     /// Toggles the completion status of a to-do item.
     func toggleTodoItemCompletion(_ item: ToDoItem) {
         if let index = allToDoItems.firstIndex(where: { $0.id == item.id }) {
@@ -39,26 +39,37 @@ final class ToDoListViewModel: ObservableObject {
             applyFilter(at: currentFilterIndex)
         }
     }
-
+    
     /// Removes a to-do item from the list.
     func removeTodoItem(_ item: ToDoItem) {
         allToDoItems.removeAll { $0.id == item.id }
         applyFilter(at: currentFilterIndex)
     }
-
+    
     /// Apply the filter to update the list.
     func applyFilter(at index: Int) {
+        // Met à jour l'index actuel du filtre
         currentFilterIndex = index
+        
+        // Utilise une instruction switch pour déterminer quel filtre appliquer
         switch index {
+            // Cas où l'index est 1 (filtre "Done" - tâches terminées)
         case 1:
+            // Met à jour toDoItems pour n'inclure que les éléments dont isDone est true
             toDoItems = allToDoItems.filter { $0.isDone }
+            
+            // Cas où l'index est 2 (filtre "Not Done" - tâches non terminées)
         case 2:
+            // Met à jour toDoItems pour n'inclure que les éléments dont isDone est false
             toDoItems = allToDoItems.filter { !$0.isDone }
+            
+            // Cas par défaut (filtre "All" - toutes les tâches)
         default:
+            // Réinitialise toDoItems pour inclure tous les éléments de allToDoItems
             toDoItems = allToDoItems
         }
     }
-
-    // Current filter index
+    
+    // Index actuel du filtre
     private var currentFilterIndex: Int = 0
 }
